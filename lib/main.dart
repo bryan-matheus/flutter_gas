eb2488283f7b01a02fbad8c7c80d4e067deafe77
Bryan <bryan.matheusbmf@gmail.com>
2021-04-22 21:27:05 -0300

    :sparkles: Added home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
-import 'package:alcool_gasolina/core/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(
+    MaterialApp(
+      home: Home(),
+    ),
+  );
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
