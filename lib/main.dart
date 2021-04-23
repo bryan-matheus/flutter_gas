b42edd8a0e7f2ac3abdd77c66d8e312ad8f0fdd2
Bryan <bryan.matheusbmf@gmail.com>
2021-04-22 21:38:35 -0300

    :sparkles: Centered title

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class _HomeState extends State<Home> {
           "Alcool  ou Gasolina",
           style: TextStyle(color: Colors.white),
         ),
+        centerTitle: true,
       ),
     );
   }
