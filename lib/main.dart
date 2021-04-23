aa4d7086f7be9ff512e261da47c0c57721efb994
Bryan <bryan.matheusbmf@gmail.com>
2021-04-22 21:31:00 -0300

    :bug: Fixed title

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Gas",
+          "Alcool  ou Gasolina",
           style: TextStyle(color: Colors.white),
         ),
       ),
